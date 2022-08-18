28c384f07e5ed2afe56ac789a7f84e249189b5de
Mehdi.ZA <mahdi_zarei78@yahoo.com>
2022-08-19 02:19:02 +0430

    add remove handler

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const app = Vue.createApp({
   methods: {
     addGoal() {
       this.goals.push(this.entredGoalValue);
+    },
+    removeGoal(index) {
+      this.goals.splice(index, 1)
     }
   }
 });
